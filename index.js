56b67341b9459ef384fd22b9100cb71a28defd61
EkinoksAhmet <80361129+EkinoksAhmet@users.noreply.github.com>
2021-10-01 20:26:56 +0300

    Formlar eklendi

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ function openPhoneOpenMenu() {
     document.getElementById("phone-open-menu").style.display = "block";
 }
 
+
+function openPhoneFormMenu() {
+    document.getElementById("phone-contact-form").style.display = "block";
+}
+
+function closePhoneFormMenu() {
+    document.getElementById("phone-contact-form").style.display = "none";
+}
+
+
+function openPhoneSponsorMenu() {
+    document.getElementById("phone-sponsor-form").style.display = "block";
+}
+
+function closePhoneSponsorMenu() {
+    document.getElementById("phone-sponsor-form").style.display = "none";
+}
