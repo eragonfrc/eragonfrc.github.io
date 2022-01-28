da30f666c35c3620c8ffc8041c193f4619eeef8f
EkinoksAhmet <80361129+EkinoksAhmet@users.noreply.github.com>
2022-01-28 19:47:38 +0300

    Navbar yapıldı, İngilizce siteler silindi
    Henüz .html ler .php olacak şekilde güncellenmedi.

---
+++ includes/extrahead.inc.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="styles.css">
+<link rel="shortcut icon" href="images/eragon_transparent.png">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
