6da22f1972445457a5d13e4a55873f000dacdf44
EkinoksAhmet <80361129+EkinoksAhmet@users.noreply.github.com>
2022-01-28 19:54:53 +0300

    Revert "Navbar yapıldı, İngilizce siteler silindi"
    This reverts commit da30f666c35c3620c8ffc8041c193f4619eeef8f.

--- includes/extrahead.inc.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="styles.css">
-<link rel="shortcut icon" href="images/eragon_transparent.png">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      rel="stylesheet">
